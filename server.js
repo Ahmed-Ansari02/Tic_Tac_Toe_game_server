d4b09991cc90090060811f711b1a7eed38aa5815
Ahmed-Ansari02 <ansari82002@gmail.com>
2022-07-29 18:21:43 -0500

    adding clients to room; and synching board state

--- server.js
+++ server.js
@@ -13,20 +13,33 @@ const io = new Server(httpserver, {
 
 let synchronised_board_state;
 let winner = null
+let available_players= []
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
 
 io.on("connection", (Socket) => {
+  io.emit("available_players", available_players)
   console.log(Socket.id + " connected");
   Socket.on("disconnect",()=>{
     console.log(Socket.id + " disconneted")
+
+    available_players= available_players.filter((value)=>value.id!==Socket.id)
+    io.emit("available_players", available_players)
   })
   Socket.on("board_state_update", (board_state)=>{
-    console.log(board_state)
+   // console.log(board_state, Socket.id)
     synchronised_board_state=board_state
+    console.log(synchronised_board_state)
     Socket.broadcast.emit("sync_board_state", synchronised_board_state)
+  })
+  Socket.on("update_players", (player_name, room_id, symbol)=>{
+    
+    available_players.push({room_id: Socket.id, name:player_name, symbol:symbol})
+    console.log(room_id)
+    Socket.join(available_players.filter(value=>value.room_id===room_id))
+    Socket.broadcast.emit("available_players", available_players)
   })
 });
 
