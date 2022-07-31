9a40565ab847f68ba754e09b9eb103d2ec81e91e
Ahmed-Ansari02 <ansari82002@gmail.com>
2022-07-30 21:01:06 -0500

    Moved winner calculation from client to server, synced board state sucessfully by maintaining a copy of the game in a unique id

--- server.js
+++ server.js
@@ -12,17 +12,50 @@ const io = new Server(httpserver, {
   },
 });
 
+function calculate_winner(arr) {
+  for (let i = 0; i < 3; i++) {
+    let index = 3 * i;
+    if (!arr[index]) {
+      continue;
+    }
+    if (arr[index] === arr[index + 1] && arr[index] === arr[index + 2]) {
+      console.log("winner is (horizontally)" + arr[index]);
+      return arr[index];
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    let index = i;
+    if (!arr[index]) {
+      continue;
+    }
+    if (arr[index] === arr[index + 3] && arr[index] === arr[index + 6]) {
+      console.log("winner is (vertically)" + arr[index]);
+      return arr[index];
+    }
+  }
+  if (arr[0] === arr[4] && arr[0] === arr[8] && arr[0] !== null) {
+    console.log("winner (diagonally)" + arr[0]);
+    return arr[0];
+  }
+
+  if (arr[2] === arr[4] && arr[2] === arr[6] && arr[2] !== null) {
+    console.log("winner (diagonally)" + arr[2]);
+    return arr[2];
+  }
+}
+
 let available_players = [];
+let board_states = {};
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
 
 io.on("connection", (Socket) => {
-  let synchronised_board_state;
   let winner = null;
-  let curr_player = "X";
+  let symbol;
   let assigned_room;
+
   Socket.emit("available_players", available_players);
 
   Socket.on("disconnect", () => {
@@ -34,23 +67,24 @@ io.on("connection", (Socket) => {
     io.to(assigned_room).emit("Ready", false);
     io.emit("available_players", available_players);
   });
-  Socket.on("board_state_update", (board_state) => {
+
+  Socket.on("board_state_update", (id) => {
     if (io.sockets.adapter.rooms.get(assigned_room).size === 2) {
-      synchronised_board_state = board_state;
-      console.log(synchronised_board_state)
-      curr_player=curr_player === "X" ? "O" : "X";
+      board_states[assigned_room][id] = symbol;
+      winner = calculate_winner(board_states[assigned_room]);
       io.to(assigned_room).emit(
         "sync_board_state",
-        synchronised_board_state,
-        curr_player
+        board_states[assigned_room],
+        winner
       );
-      console.log(curr_player)
+      console.log(board_states, winner, symbol);
 
       return;
     }
     Socket.emit("sync_board_state", "wait for players");
   });
-  Socket.on("update_players", (player_name, room_id, symbol) => {
+
+  Socket.on("update_players", (player_name, room_id, symbol_selected) => {
     if (available_players.length !== 0) {
       console.log(room_id);
       Socket.join(room_id);
@@ -60,23 +94,29 @@ io.on("connection", (Socket) => {
       available_players = available_players.filter(
         (value) => value.room_id !== room_id
       );
+
       Socket.rooms.forEach((element) => {
         assigned_room = element;
       });
-      io.to(assigned_room).emit("Ready", true, assigned_room);
+      symbol = symbol_selected;
+      io.to(assigned_room).emit("Ready", true);
       return;
     }
     available_players.push({
       room_id: Socket.id,
       name: player_name,
-      symbol: symbol,
+      symbol: symbol_selected,
     });
+    symbol = symbol_selected;
+
     Socket.rooms.forEach((element) => {
       assigned_room = element;
-      console.log("in room " + element);
+      board_states[assigned_room] = new Array(9).fill(null);
+      console.log("in room " + assigned_room);
     });
 
     Socket.broadcast.emit("available_players", available_players);
+    console.log(board_states);
   });
 });
 
